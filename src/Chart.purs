785ad9ebdcb059504416174b2a047f7352af0246
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2017-02-22 23:34:54 +0000

    Initial version

---
+++ src/Chart.purs
@@ -0,0 +1,56 @@
+module Chart where
+
+import Prelude
+import CSS (backgroundColor, color, display, div, fromString, height, inlineBlock, margin, paddingLeft, px, rgb, steelblue, width) as CSS
+import CSS.VerticalAlign (textBottom, verticalAlign) as CSS
+import Halogen.HTML as H
+import Halogen.HTML.Properties as P
+import CSS ((?))
+import Data.Foldable (maximum)
+import Data.Formatter.Number (formatOrShowNumber)
+import Data.Maybe (fromMaybe)
+import Data.Traversable (sum)
+import Halogen (ComponentHTML)
+import Halogen.HTML.CSS (style, stylesheet)
+
+type Range = { min:: Number, max:: Number }
+type Scale = Number -> Number
+
+linearScale :: Range -> Range -> Scale
+linearScale { min, max } { min: rangeMin, max: rangeMax } x =
+  (x - min) / (max - min) * (rangeMax - rangeMin) + rangeMin
+  where
+    x' = case x of
+      _ | x < min -> min
+      _ | x > max -> max
+      _ | otherwise -> x
+
+data F a = F a
+
+renderDays :: Array Number -> ComponentHTML F
+renderDays input =
+  H.div_ $ [
+      stylesheet do
+        CSS.div ? CSS.fromString ".bar" ? do
+          CSS.backgroundColor CSS.steelblue
+          CSS.margin (CSS.px 1.0) (CSS.px 1.0) (CSS.px 1.0) (CSS.px 1.0)
+          CSS.display CSS.inlineBlock
+    ] <>
+    map (\n -> H.div [
+      P.class_ (H.className "bar"),
+      style do
+        CSS.width (CSS.px 8.0)
+        CSS.height (CSS.px $ ysc n)
+      ] []) input <>
+    [ H.span [
+        style do
+          CSS.color (CSS.rgb 120 120 120)
+          CSS.paddingLeft (CSS.px 5.0)
+          CSS.verticalAlign CSS.textBottom
+      ]
+      [ H.text $ formatOrShowNumber "00.0" ((sum input) /1000.0) <> "km" ]
+    ]
+
+  where
+
+  ysc = linearScale { min: 0.0, max: fromMaybe 0.0 $ maximum input } { min: 0.0, max: 30.0 }
---
+++ src/Main.purs
@@ -0,0 +1,71 @@
+module Main where
+
+import Prelude
+import Chart (renderDays)
+import Control.Monad.Aff (Aff, launchAff)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Data.Array (filter, groupBy, mapMaybe, nub, range, (:))
+import Data.Date (Date, Month, Weekday(..), canonicalDate, weekday)
+import Data.DateTime (DateTime(DateTime), adjust, date)
+import Data.Either (Either(..))
+import Data.Enum (toEnum)
+import Data.Foldable (sum)
+import Data.Formatter.DateTime (format, parseFormatString)
+import Data.Int (toNumber)
+import Data.Map (Map, fromFoldable, lookup)
+import Data.Maybe (Maybe, maybe)
+import Data.Newtype (unwrap)
+import Data.NonEmpty ((:|))
+import Data.Time.Duration (Days(..))
+import Data.Traversable (for_)
+import Data.Tuple (Tuple(..))
+import Halogen.HTML.Renderer.String (renderHTML)
+import Network.HTTP.Affjax (AJAX)
+import Node.Encoding (Encoding(..))
+import Node.FS (FS)
+import Node.FS.Aff (writeTextFile)
+import Node.Process (PROCESS, argv)
+import Partial.Unsafe (unsafePartial)
+import Strava.API.Activities (ActivityRangeStart(..), AuthToken(..), getMany)
+import Strava.Activity (Activity, ActivityType(..), Metres)
+
+date' :: Int -> Month -> Int -> Maybe Date
+date' year month day =
+  canonicalDate <$> toEnum year <*> pure month <*> toEnum day
+
+week :: Date -> Array Date
+week d = map date $ mapMaybe (flip adjust $ DateTime d bottom) $ Days <$> toNumber <$> range (-6) 0
+
+actDate :: Activity -> Date
+actDate = date <<< unwrap <<< _.startDate <<< unwrap
+
+generateCharts :: forall eff. AuthToken -> String -> Aff (console :: CONSOLE, fs :: FS, ajax :: AJAX | eff) Unit
+generateCharts token outputDir = do
+  acts <- getMany token { start: Default, pageCount: 50 }
+  let runs = filter ((==) Run <<< _.type' <<< unwrap) acts
+      sundays = filter ((==) Sunday <<< weekday) $ nub $ (date <<< unwrap <<< _.startDate <<< unwrap) <$> runs
+      byDate = groupBy (\a b -> actDate a == actDate b) runs
+      totalMap :: Map Date Metres
+      totalMap = fromFoldable $ (\(r:|runs) -> Tuple (actDate r) (sum $ (_.distance <<< unwrap) <$> r:runs )) <$> byDate
+      forWeek :: Date -> Array Number
+      forWeek d = (maybe 0.0 unwrap) <$> flip lookup totalMap <$> week d
+  for_ sundays $ \d -> do
+    let html = renderHTML (renderDays $ forWeek d)
+        dateStr = format <$> (parseFormatString "YYYY-MM-DD") <*> pure (DateTime d bottom)
+    case dateStr of
+      Left _ -> pure unit
+      Right s -> do
+        liftEff $ log $ s <> ": " <> show (forWeek d)
+        let filename = outputDir <> "/" <> s <> ".html"
+        liftEff $ log $ "Wrote file: " <> filename
+        writeTextFile UTF8 filename html
+
+main :: forall a. Eff (err :: EXCEPTION, console :: CONSOLE, fs :: FS, ajax :: AJAX, process :: PROCESS | a) Unit
+main = void $ launchAff $ unsafePartial $ do
+  args <- liftEff argv
+  case args of
+    [ _, _, token, outDir ] -> generateCharts (AuthToken token) outDir
+    _ -> liftEff $ log $ "Usage: weekchart <token> <outputDir>"
